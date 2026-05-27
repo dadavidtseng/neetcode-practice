f90ba92c67b86c7616f78b5e738b74cc924625c0
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-05-27 18:43:22 -0500

    Add: binary-tree-maximum-path-sum - submission-1

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-1.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+   public:
+    int maxPathSum(TreeNode* root) {
+        // return 0 if root node is nullptr to prevent crashing
+        if (root == nullptr) {
+            return 0;
+        }
+
+        // initialize the result to INT_MIN for all negative nodes case
+        int result = INT_MIN;
+
+        // pass the result by reference in to helper function for recursive call
+        maxPathSum(root, result);
+
+        return result;
+    }
+
+    int maxPathSum(TreeNode const* node, int& sum) {
+        // return 0 if this node is nullptr to prevent crashing
+        if (node == nullptr) {
+            return 0;
+        }
+
+        TreeNode const* cur = node;
+
+        // get the best chain from the left/right subtree, ignore if negative
+        // leftSum/rightSum is equal to the outcome if it's greater than 0
+        int const leftSum = max(0, maxPathSum(cur->left, sum));
+        int const rightSum = max(0, maxPathSum(cur->right, sum));
+
+        // update the passed in sum
+        // if leftSum + cur->val + rightSum is greater than sum
+        // this is the only thing current node cases about
+        sum = max(sum, leftSum + cur->val + rightSum);
+
+        // return current node's value + whichever is greater among leftSum and rightSum
+        return cur->val + max(leftSum, rightSum);
+    }
+};
