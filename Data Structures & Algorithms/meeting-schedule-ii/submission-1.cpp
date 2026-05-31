80ba518a6f78a1a31fc4ffdb653475e08985576d
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-05-31 10:01:00 -0500

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        int result    = 0;
+        int maxResult = 0;
+
+        int const     n = static_cast<int>(intervals.size());
+        map<int, int> events;
+
+        for (int i = 0; i < n; ++i)
+        {
+            events[intervals[i].start]++;
+            events[intervals[i].end]--;
+        }
+
+        for (map<int, int>::iterator it = events.begin(); it != events.end(); ++it)
+        {
+            result += it->second;
+            maxResult = max(maxResult, result);
+        }
+        
+        return maxResult;
+    }
+};
