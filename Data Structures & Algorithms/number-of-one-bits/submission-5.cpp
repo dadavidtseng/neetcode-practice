d31485af3d3ead9d7d4395518b2e86a74900f156
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-15 16:58:55 -0500

    Add: number-of-one-bits - submission-5

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+    int hammingWeight(uint32_t n) {
+        int result = 0;
+
+        // Exit the while loop when n isn't 0
+        while (n != 0) {
+            // Flip the rightmost 1 bit to 0
+            // Note that n | (n + 1) can flip the rightmost 0 bit to 1
+            n = n & (n - 1);
+            result++;
+        }
+        return result;
+    }
+};
