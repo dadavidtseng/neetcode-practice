4e0e9363e7d0399fe6dcb36a524b45454042372c
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-23 10:22:22 -0500

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        mask = 0xFFFFFFFF
+        result = n
+
+        result = (result >> 16) | (result << 16) & mask
+        result = ((result & 0xFF00FF00) >> 8) | ((result & 0x00FF00FF) << 8)
+        result = ((result & 0xF0F0F0F0) >> 4) | ((result & 0x0F0F0F0F) << 4)
+        result = ((result & 0xCCCCCCCC) >> 2) | ((result & 0x33333333) << 2)
+        result = ((result & 0xAAAAAAAA) >> 1) | ((result & 0x55555555) << 1)
+
+        return result & mask
