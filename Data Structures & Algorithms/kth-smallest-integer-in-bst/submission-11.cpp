c06376e206c260d3fbc25aa5738bc63a5287e967
Yu-Wei Tseng <113273083+dadavidtseng@users.noreply.github.com>
2026-07-26 14:34:45 -0500

    Add: kth-smallest-integer-in-bst - submission-11

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-11.cpp
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+   public:
+    int kthSmallest(TreeNode* root, int k) {
+        TreeNode* curr = root;
+
+        // Exit the while loop when there's no more node to process
+        while (curr != nullptr) {
+            // If current node doesn't have a left child,
+            // 1. Decrement k
+            // 2. Return current node's value if k is 0
+            // 3. Move the current node to its right
+            if (curr->left == nullptr) {
+                k--;
+
+                if (k == 0) {
+                    return curr->val;
+                }
+                curr = curr->right;
+            }
+            // If current node does have a left child
+            else {
+                // Assign that left child node as predecessor
+                TreeNode* pred = curr->left;
+
+                // Find the predecessor node
+                while (pred->right != nullptr && pred->right != curr) {
+                    pred = pred->right;
+                }
+
+                // If this is the first time we visit the predecessor
+                // 1. Create a link back to current node so that the next time we visit we know
+                // where the root is for inorder traversal
+                // 2. Move the current node to its left
+                if (pred->right == nullptr) {
+                    pred->right = curr;
+                    curr = curr->left;
+                }
+                // If this is the second time we visit the predecessor,
+                // it's time to go back to root node
+                // 1. Rremove the link from predecessor to current node
+                // 2. Move the current node to its right
+                else {
+                    pred->right = nullptr;
+                    k--;
+
+                    if (k == 0) {
+                        return curr->val;
+                    }
+                    curr = curr->right;
+                }
+            }
+        }
+        // Return -1 to satisfy the compiler
+        // Note that we'll never reach this because 1 <= k <= n <= 10^4
+        return -1;
+    }
+};
